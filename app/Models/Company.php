f55545dc2c1bd017e7fae4cf0bdc00f33447a0cd
Romulo Xavier <romulo.xavier25@gmail.com>
2024-11-26 12:13:32 -0300

    feat: criando migration de company e implementando as colunas que podem ser modificada.

---
+++ app/Models/Company.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $fillable = [
+        'name', 'url', 'phone', 'whatsapp', 'email',
+        'facebook', 'instagram', 'youtube', 'is_active'
+    ];
+}
